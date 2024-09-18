eaa4ba87504b1fa7e560cc167b773915d3d1a039
rivehh <mariana_medeiros.2016@hotmail.com>
2024-09-18 18:16:07 -0300

    Tela de mapa

--- src/screens/Rota.tsx
+++ src/screens/Rota.tsx
@@ -1,9 +1,20 @@
-import { StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, View, TextInput} from 'react-native';
 import { Feather } from '@expo/vector-icons';
+import React, { useState } from 'react';
 
 export default function Mapa() {
+
+  const [text, setText] = useState('');
+  const [isFocused, setIsFocused] = useState(false);
+
   return (
     <View style={styles.container}>
+      <TextInput style={styles.campo}
+      placeholder={isFocused ? '' : '  Pesquisar rota'}
+      value={text}
+      onChangeText={setText}
+      onFocus={() => setIsFocused(true)}
+      onBlur={() => setIsFocused(false)}/>
     </View>
   );
 }
@@ -14,6 +25,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     display: 'flex'
+  },
+  campo: {
+    backgroundColor: 'white',
+    borderRadius: 25,
+    fontSize: 15,
+    color: 'black',
+    padding: 12,
+    width: '75%',
+    height: 50,
+    alignSelf: 'center',
+    elevation: 7,
+    paddingVertical: 10,
+    borderWidth: 0.8, 
+    position: 'absolute',
+    bottom: 0,
+    marginBottom: 20
   },
 
 });
